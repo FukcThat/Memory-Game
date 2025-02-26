f2d59b07682781d27426577242e0fd3163244ffa
FukcThat <63652e636f6465@gmail.com>
2025-02-26 19:42:28 +0100

    ADD: Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import Tilt from "react-parallax-tilt";
+import "./styles/Card.css";
+
+function Card({ character }) {
+  return (
+    <Tilt
+      glareEnable={true}
+      glareMaxOpacity={0.4}
+      glareColor="#ffffff"
+      glarePosition="bottom"
+      className="tilt"
+    >
+      <div className="card">
+        <img src={character.gif} alt={character.name} className="card-image" />
+        <div className="card-name">{character.name}</div>
+      </div>
+    </Tilt>
+  );
+}
+
+export default Card;
