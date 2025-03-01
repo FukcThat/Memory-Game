09ac37fa28db625849d20e9e5f983dd15041120f
FukcThat <63652e636f6465@gmail.com>
2025-03-01 21:25:05 +0100

    ADD: Game Screen component:

---
+++ src/screens/GameScreen.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function GameScreen() {
+  return (
+    <>
+      <section className="game-screen">
+        <Header />
+        {/* Card Section */}
+        <Footer />
+      </section>
+    </>
+  );
+}
