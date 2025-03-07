faa79d6614197ec0f49e47ec8e3e84a6e49a74c7
FukcThat <63652e636f6465@gmail.com>
2025-03-07 13:52:03 +0100

    ADD: All characters

--- src/data/characters.js
+++ src/data/characters.js
@@ -20,4 +20,65 @@ export const initialCharacters = [
       "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjJzbHN1ZWNhMTJwNGJjdHZqdGhza2UyZG02eHgyOTcxa3h5M3EwayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qKmWi9mfycx6E/giphy.gif",
     ],
   },
+  {
+    id: 4,
+    name: "Kodama",
+    gifOptions: [
+      "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTEwMzA4aXVjc2FjZ2Z0YXc4YW94c253cm1yaGtjNHB5bzNnanFnaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/c2ABhPQs7uUYo/giphy.gif",
+    ],
+  },
+  {
+    id: 5,
+    name: "Jiji",
+    gifOptions: [
+      "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDl2ZzNoNHFtbDlpNDlrcjc3bDNic24zMXIwd2FmYzE2ZnZocnptayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9SVdZvlJYzTdS/giphy.gif",
+    ],
+  },
+  {
+    id: 6,
+    name: "Ohmu",
+    gifOptions: [
+      "https://64.media.tumblr.com/tumblr_lv5wfjwtk81qa5852o1_500.gif",
+    ],
+  },
+  {
+    id: 7,
+    name: "Turnip Head",
+    gifOptions: [
+      "https://media1.tenor.com/m/qmGy-4YrOZQAAAAd/turniphead-miyazaki.gif",
+    ],
+  },
+  {
+    id: 8,
+    name: "Kamaji",
+    gifOptions: [
+      "https://i.pinimg.com/originals/4d/49/f6/4d49f671460a57d73fc8a56a11bcbd45.gif",
+    ],
+  },
+  {
+    id: 9,
+    name: "Suigyo",
+    gifOptions: [
+      "https://64.media.tumblr.com/9a957249cb1f4bea9193fe12db6a0b21/tumblr_mjobnfMQgm1raym3wo1_500.gif",
+    ],
+  },
+  {
+    id: 10,
+    name: "Heen",
+    gifOptions: ["https://i.imgur.com/EQRL1hY.gif"],
+  },
+  {
+    id: 11,
+    name: "Warawara",
+    gifOptions: [
+      "https://64.media.tumblr.com/62368b7e5f57ed0799f75014e5e581ec/0ffa54c03b441436-d9/s540x810/1523971bbf27dd857cf94e3f2260ff5e302354fd.gif",
+    ],
+  },
+  {
+    id: 12,
+    name: "Prince Lune",
+    gifOptions: [
+      "https://64.media.tumblr.com/2b7610691532d4b9b5d872f982e7aed4/tumblr_otjmbu4r5p1thtrsvo2_400.gif",
+    ],
+  },
 ];
