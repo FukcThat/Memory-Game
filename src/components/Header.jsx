4b0438a93ef07c062d5af173d88e7434c664704e
FukcThat <63652e636f6465@gmail.com>
2025-03-12 20:59:12 +0100

    STYLE: Start screen layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import Rules from "./Rules";
+import { useState } from "react";
+import "/src/styles/Header.css";
 
 export default function Header() {
+  const [showRules, setShowRules] = useState(false);
+
   return (
     <div className="header">
       <div>{/* Logo onclick back to home*/}</div>
-      <Rules />
+
+      <button className="rules-btn" onClick={() => setShowRules(!showRules)}>
+        {showRules ? "x" : "?"}
+      </button>
+      {showRules && <Rules />}
     </div>
   );
 }
--- src/screens/StartScreen.jsx
+++ src/screens/StartScreen.jsx
@@ -1,20 +1,24 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import "/src/styles/StartScreen.css";
 
 export default function StartScreen({ onStart, difficulties }) {
   return (
-    <div className="home-screen">
+    <div className="start-screen">
       <Header />
       <h1>Welcome to Spirit Parade Memory!</h1>
       <p>Select your difficulty:</p>
       <div className="difficulty-btn-group">
-        {difficulties.map(([level, { cardCount }]) => (
-          <button key={level} onClick={() => onStart(level)}>
-            {level[0].toUpperCase() + level.slice(1)} ({cardCount} cards)
+        {difficulties.map(([level]) => (
+          <button
+            className="difficulty-btn"
+            key={level}
+            onClick={() => onStart(level)}
+          >
+            {level[0].toUpperCase() + level.slice(1)}
           </button>
         ))}
       </div>
-      <button className="start-btn">Start</button>
       <Footer />
     </div>
   );
